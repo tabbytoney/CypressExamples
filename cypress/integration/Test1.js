05e3962d001c47e187177a4ecf6e2ba3b29fa421
Tabby Toney <tabbytoney@gmail.com>
2020-09-01 19:11:39 -0500

    First commit of Udemy Cypress tutorial

---
+++ cypress/integration/Test1.js
@@ -0,0 +1,10 @@
+describe('My First Test Suite', function () {
+
+    it('My First Test Case', function () {
+        //test steps go inside it block
+        cy.visit("www.sexdrugsandhistory.com");
+    })
+    //you can have more than one test case within an it block, it would be placed like the next line
+    //it('My Second Test Case... ')
+
+})
