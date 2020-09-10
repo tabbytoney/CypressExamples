ceec030b188e36647d02f1c2c322a41ff90a47f2
Tabby Toney <tabbytoney@gmail.com>
2020-09-09 22:51:57 -0500

    Further into Udemy tutorial with assertions

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -2,9 +2,30 @@ describe('My First Test Suite', function () {
 
     it('My First Test Case', function () {
         //test steps go inside it block
-        cy.visit("www.sexdrugsandhistory.com");
-    })
-    //you can have more than one test case within an it block, it would be placed like the next line
-    //it('My Second Test Case... ')
+        cy.visit("https://www.rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        //in Cypress, get acts like 'find'
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length', 4)
+        //Parent child chaining
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+
+            const textVeg = $el.find('h4.product-name').text()
+            if (textVeg.includes('Cashews')) {
+                $el.find('button').click()
+            }
+        })
+        //handling promises manually
+        cy.get('.brand').then(function (logoelement) {
+            cy.log(logoelement.text())
+        })
+
+
+
+
+    })
 })
+
